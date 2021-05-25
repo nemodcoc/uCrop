cd55793dfc6b507140f6645e9804f813af10dc68
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2021-05-25 15:21:15 +0200

    Update BitmapCropTask.java

--- ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
+++ ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
@@ -188,7 +188,9 @@ public class BitmapCropTask extends AsyncTask<Void, Void, Throwable> {
         if (mCropCallback != null) {
             if (t == null) {
                 Uri uri = Uri.fromFile(new File(mImageOutputPath));
-                mCropCallback.onBitmapCropped(uri, cropOffsetX, cropOffsetY, mCroppedImageWidth, mCroppedImageHeight);
+                //Mariano.py
+                //mCropCallback.onBitmapCropped(uri, cropOffsetX, cropOffsetY, mCroppedImageWidth, mCroppedImageHeight);
+                mCropCallback.onBitmapCropped(uri, cropOffsetX, cropOffsetY, mCroppedImageWidth, mCroppedImageHeight, mCurrentAngle);
             } else {
                 mCropCallback.onCropFailure(t);
             }
