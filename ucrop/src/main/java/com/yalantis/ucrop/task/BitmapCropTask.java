e11168f7365e1e03317cce87fa4985234d6fc2f0
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2021-05-25 10:18:29 +0200

    Update BitmapCropTask.java

--- ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
+++ ucrop/src/main/java/com/yalantis/ucrop/task/BitmapCropTask.java
@@ -136,7 +136,7 @@ public class BitmapCropTask extends AsyncTask<Void, Void, Throwable> {
         mCroppedImageWidth = Math.round(mCropRect.width() / mCurrentScale);
         mCroppedImageHeight = Math.round(mCropRect.height() / mCurrentScale);
 
-        Log.i("ProvaMariano.py", Float.toString(mCurrentAngle));
+        Log.i("Angolo in BitmapCropTask - Mariano.py", Float.toString(mCurrentAngle));
         
         boolean shouldCrop = shouldCrop(mCroppedImageWidth, mCroppedImageHeight);
         Log.i(TAG, "Should crop: " + shouldCrop);
